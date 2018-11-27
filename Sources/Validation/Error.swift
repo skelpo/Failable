635c689db2cb64573e2bacb117e2b70f1c02c048
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-11-27 16:31:08 -0600

    Created ValidationError struct

---
+++ Sources/Validation/Error.swift
@@ -0,0 +1,23 @@
+/// An error that occurs while validating a value.
+public struct ValidationError: Error {
+    
+    /// The machine readable ID for the error.
+    public let identifier: String
+    
+    /// The human readable reason that the error occured.
+    public let reason: String
+    
+    /// Creates a new `ValidationError` instance.
+    ///
+    /// - Parameters:
+    ///   - identifier: The machine readable ID for the error.
+    ///   - reason: The human readable reason that the error occured.
+    public init(identifier: String, reason: String) {
+        self.identifier = identifier
+        self.reason = reason
+    }
+    
+    /// The error that is thrown when the value passed into the type-erased
+    /// `AnyValidation.validate` method cannot be cast to the `Validation.Support` type.
+    public static let invalidType = ValidationError(identifier: "invalidType", reason: "Cannot convert Any to validation supported type")
+}
