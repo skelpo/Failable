cc059df288128da63e30e1c89c9cb424e922b60f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-11-28 09:20:11 -0600

    Created test cases for ValidationTests suite

--- Tests/ValidationTests/ValidationTests.swift
+++ Tests/ValidationTests/ValidationTests.swift
@@ -5,6 +5,57 @@ internal struct EmptyValidation<T>: Validation {
     typealias Supported = T
 }
 
+internal struct ValidationList: Validation {
+    typealias Supported = Bool
+    
+    static let subvalidations: [AnyValidation.Type] = [
+        EmptyValidation<Bool>.self,
+        EmptyValidation<String>.self,
+        EmptyValidation<Int>.self,
+        EmptyValidation<Bool>.self,
+        EmptyValidation<Bool>.self,
+        EmptyValidation<[Bool]>.self,
+        EmptyValidation<[String: Any]>.self,
+        EmptyValidation<Set<Float>>.self
+    ]
+}
+
 final class ValidationTests: XCTestCase {
-    static var allTests: [(String, (ValidationTests) -> ()throws -> ())] = []
+    func testType()throws {
+        let validation = EmptyValidation<Bool>.self
+        
+        XCTAssert(validation.type == Bool.self)
+        XCTAssert(validation.Supported.self == Bool.self)
+    }
+    
+    func testDefaultImplementations()throws {
+        let validation = EmptyValidation<Bool>.self
+        
+        XCTAssertEqual(validation.subvalidations.count, 0)
+        XCTAssertEqual(validation.safeSubvalidations.count, 0)
+        try XCTAssertNoThrow(validation.validate(false))
+        try XCTAssertNoThrow(validation.validate(true))
+        
+        do {
+            try validation.validate("Fail")
+        } catch let error as ValidationError {
+            XCTAssertEqual(error.identifier, "invalidType")
+            XCTAssertEqual(error.reason, "Cannot convert Any to validation supported type")
+        } catch let error {
+            XCTFail("Unexpected error: " + error.localizedDescription)
+        }
+    }
+    
+    func testSafeSubvalidations()throws {
+        XCTAssertEqual(ValidationList.safeSubvalidations.count, 3)
+        for validation in ValidationList.safeSubvalidations {
+            XCTAssert(validation.type == Bool.self)
+        }
+    }
+    
+    static var allTests: [(String, (ValidationTests) -> ()throws -> ())] = [
+        ("testType", testType),
+        ("testDefaultImplementations", testDefaultImplementations),
+        ("testSafeSubvalidations", testSafeSubvalidations)
+    ]
 }
