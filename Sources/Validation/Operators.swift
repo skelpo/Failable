6db974df1447c93d23af50d73765956b968f4ff1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-11-28 06:49:18 -0600

    Use type checked sub-validations in <~ operator instead of all sub-validations

--- Sources/Validation/Operators.swift
+++ Sources/Validation/Operators.swift
@@ -4,11 +4,15 @@ infix operator <~
 ///
 /// You have to use this operator instead of `=` so the validations always run on the new value before it is assigned.
 ///
+/// - Note: Sub-validations are only run 1 level deep. This prevents any validation loops that could occur and saves on run time.
+///
+/// - Complexity: O(n), where _n_ is the number of type compatible sub-validations for the `Failable` type's sub-validations.
+///
 /// - Parameters:
 ///   - root: The `Failable` instance that holds the value to be mutated.
 ///   - value: The new value for the `root.value` property.
 public func <~ <T, Validations>(root: inout Failable<T, Validations>, value: T)throws {
     try Validations.validate(value)
-    try Validations.subvalidations.forEach { validation in try validation.validate(value) }
+    try Validations.safeSubvalidations.forEach { validation in try validation.validate(value) }
     root.value = value
 }
