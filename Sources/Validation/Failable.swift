040b12371df0770877a272a30311ee25f6e9f293
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-11-28 10:47:42 -0600

    Moved Failable+Codable conformance to Failable+Codable.swift file

--- Sources/Validation/Failable.swift
+++ Sources/Validation/Failable.swift
@@ -36,24 +36,6 @@ public struct Failable<T, Validations> where Validations: Validation, Validation
     }
 }
 
-extension Failable: Encodable where T: Encodable {
-    
-    /// See [`Encodable.encode(to:)`](https://developer.apple.com/documentation/swift/encodable/2893603-encode)
-    public func encode(to encoder: Encoder)throws {
-        var container = encoder.singleValueContainer()
-        try container.encode(self.value)
-    }
-}
-
-extension Failable: Decodable where T: Decodable {
-    
-    /// See [`Decodable.init(from:)`](https://developer.apple.com/documentation/swift/decodable/2894081-init)
-    public init(from decoder: Decoder)throws {
-        let container = try decoder.singleValueContainer()
-        try self.init(container.decode(T.self))
-    }
-}
-
 extension CustomStringConvertible {
     
     /// Easily create `Failable` versions of types conforming to `CustomStringConvertible`. This includes most, if not all, core Swift types.
