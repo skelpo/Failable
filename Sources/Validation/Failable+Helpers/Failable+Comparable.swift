a48c3acbe02254efbe32125c17d79fe3eb10e84f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-11-28 11:17:39 -0600

    Added additional comperison operators for Failable types with same underlying types and different validations

--- Sources/Validation/Failable+Helpers/Failable+Comparable.swift
+++ Sources/Validation/Failable+Helpers/Failable+Comparable.swift
@@ -1,3 +1,5 @@
+// MARK: - Equatable
+
 extension Failable: Equatable where T: Equatable {
     
     /// See [`Equatable.==(-:_:)`](https://developer.apple.com/documentation/swift/equatable/1539854).
@@ -6,7 +8,8 @@ extension Failable: Equatable where T: Equatable {
     }
 }
 
-/// Compares two `Failable` instances where type `T` is the same and comforms to `Equatable`, but the `Validations` are different.
+/// Checks for equality of the `value` property from two `Failable` instances where type `T` is the same and comforms to `Equatable`,
+/// but the `Validations` are different.
 ///
 /// - Parameters:
 ///   - lhs: The left `Failable` instance to compare.
@@ -15,10 +18,63 @@ public func == <T, V1, V2>(lhs: Failable<T, V1>, rhs: Failable<T, V2>) -> Bool w
     return lhs.value == rhs.value
 }
 
+/// Checks for inequality of the `value` property from two `Failable` instances where type `T` is the same and comforms to `Equatable`,
+/// but the `Validations` are different.
+///
+/// - Parameters:
+///   - lhs: The left `Failable` instance to compare.
+///   - rhs: The right `Failable` instance to compare.
+public func != <T, V1, V2>(lhs: Failable<T, V1>, rhs: Failable<T, V2>) -> Bool where T: Equatable {
+    return lhs.value != rhs.value
+}
+
+// MARK: - Comparable
+
 extension Failable: Comparable where T: Comparable {
     
     /// See [`Comparable.<(_:_:)`](https://developer.apple.com/documentation/swift/comparable/1538311)
     public static func < (lhs: Failable<T, Validations>, rhs: Failable<T, Validations>) -> Bool {
         return lhs.value < rhs.value
     }
+}
+
+/// Checks that the `value` property from one `Failable` instance is great than another, where type `T` is the same and comforms to `Comparable`,
+/// but the `Validations` are different.
+///
+/// - Parameters:
+///   - lhs: The left `Failable` instance to compare.
+///   - rhs: The right `Failable` instance to compare.
+public func > <T, V1, V2>(lhs: Failable<T, V1>, rhs: Failable<T, V2>) -> Bool where T: Comparable {
+    return lhs.value > rhs.value
+}
+
+/// Checks that the `value` property from one `Failable` instance is less than another, where type `T` is the same and comforms to `Comparable`,
+/// but the `Validations` are different.
+///
+/// - Parameters:
+///   - lhs: The left `Failable` instance to compare.
+///   - rhs: The right `Failable` instance to compare.
+public func < <T, V1, V2>(lhs: Failable<T, V1>, rhs: Failable<T, V2>) -> Bool where T: Comparable {
+    return lhs.value < rhs.value
+}
+
+
+/// Checks that the `value` property from one `Failable` instance is great than or equal to another,
+/// where type `T` is the same and comforms to `Comparable`, but the `Validations` are different.
+///
+/// - Parameters:
+///   - lhs: The left `Failable` instance to compare.
+///   - rhs: The right `Failable` instance to compare.
+public func >= <T, V1, V2>(lhs: Failable<T, V1>, rhs: Failable<T, V2>) -> Bool where T: Comparable {
+    return lhs.value >= rhs.value
+}
+
+/// Checks that the `value` property from one `Failable` instance is less than or equal to another,
+/// where type `T` is the same and comforms to `Comparable`, but the `Validations` are different.
+///
+/// - Parameters:
+///   - lhs: The left `Failable` instance to compare.
+///   - rhs: The right `Failable` instance to compare.
+public func <= <T, V1, V2>(lhs: Failable<T, V1>, rhs: Failable<T, V2>) -> Bool where T: Comparable {
+    return lhs.value <= rhs.value
 }
