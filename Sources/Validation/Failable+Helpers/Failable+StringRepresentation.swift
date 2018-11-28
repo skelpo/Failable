109792780183f8d5699f2752829ad0ed1cacf7fe
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-11-28 11:23:29 -0600

    Conformed Failable<LosslessStringConvertible, T> type to LosslessStringConvertible protocol

--- Sources/Validation/Failable+Helpers/Failable+StringRepresentation.swift
+++ Sources/Validation/Failable+Helpers/Failable+StringRepresentation.swift
@@ -5,3 +5,18 @@ extension Failable: CustomStringConvertible where T: CustomStringConvertible {
         return "Failable(" + self.value.description + ")"
     }
 }
+
+extension Failable: LosslessStringConvertible where T: LosslessStringConvertible {
+    
+    /// See [`LosslessStringConvertible.init(_:)`](https://developer.apple.com/documentation/swift/losslessstringconvertible/2429639-init).
+    ///
+    /// If the value passed into the `Failable` initializer throws an error, the initializer with return `nil`.
+    public init?(_ description: String) {
+        guard let value = T(description) else { return nil }
+        do {
+            try self.init(value)
+        } catch {
+            return nil
+        }
+    }
+}
